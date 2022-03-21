f930a3fba974de039554fccf79168f6c7df6374c
Madhav Bhasin <madhav.bhasin@devslane.com>
2022-03-21 20:11:54 +0530

    [FIX] add args for spinner story

--- src/spinner/Spinner.stories.tsx
+++ src/spinner/Spinner.stories.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
 import { TopRoundedSpinner } from './Spinner';
 
 export default {
   title: 'Spinner',
   component: TopRoundedSpinner,
+  argTypes: {
+    border: {
+      control: {
+        type: null,
+      },
+      type: {
+        name: 'string',
+        required: false,
+      },
+      table: {
+        defaultValue: {
+          summary: 'border-red-300',
+        },
+      },
+    },
+    customClassName: {
+      control: {
+        type: null,
+      },
+    },
+  },
 } as ComponentMeta<typeof TopRoundedSpinner>;
 
 const Template: ComponentStory<typeof TopRoundedSpinner> = (args) => (
@@ -15,22 +35,4 @@ const Template: ComponentStory<typeof TopRoundedSpinner> = (args) => (
 export const Large = Template.bind({});
 Large.args = {
   size: 'lg',
-  border: 'border-blue-200',
-};
-
-export const Medium = Template.bind({});
-Medium.args = {
-  size: 'md',
-  border: 'border-red-900',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'sm',
-  border: 'border-yellow-200',
-};
-
-export const XLarge = Template.bind({});
-XLarge.args = {
-  size: 'xl',
 };
